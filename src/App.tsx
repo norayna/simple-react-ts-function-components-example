36cbec3aec651ccba34afc13f2422de24d74d7dd
norayna <rayna.bozhkova@mayden.co.uk>
2022-04-21 18:07:20 +0100

    changed the user to be in Apps state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
 import Main from "./Main";
 import Navbar from './Navbar'
+import {useState} from "react";
 
 function App() {
-    const user = 'rayna'
+    const [user, setUser] = useState('rayna')
 
     return (
         <div className="App">
